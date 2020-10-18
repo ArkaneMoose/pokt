b5a089ccd42406d66991a18a2af60cd2abaf5a33
Rishov Sarkar <ArkaneMoose@users.noreply.github.com>
2020-10-18 02:31:43 -0400

    Random storefront stuff

--- lib/screens/profile/components/profile_list_item.dart
+++ lib/screens/profile/components/profile_list_item.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:line_awesome_flutter/line_awesome_flutter.dart';
 import 'package:pokt/constants.dart';
 import 'package:ionicons/ionicons.dart';
 
